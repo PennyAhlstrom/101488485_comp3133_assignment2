e0443e91549c93977a19bf14a57ffc6f0edb38f2
Penny Ahlstrom <pennyahlstrom@hotmail.com>
2026-04-10 22:35:20 -0400

    add graphql query and mutation documents

---
+++ src/app/graphql/employee.graphql.ts
@@ -0,0 +1,100 @@
+import { gql } from '@apollo/client/core';
+
+export const GET_EMPLOYEES_QUERY = gql`
+  query GetEmployees {
+    getEmployees {
+      id
+      first_name
+      last_name
+      email
+      gender
+      designation
+      salary
+      date_of_joining
+      department
+      employee_photo
+      created_at
+      updated_at
+    }
+  }
+`;
+
+export const GET_EMPLOYEE_BY_ID_QUERY = gql`
+  query GetEmployeeById($id: ID!) {
+    getEmployeeById(id: $id) {
+      id
+      first_name
+      last_name
+      email
+      gender
+      designation
+      salary
+      date_of_joining
+      department
+      employee_photo
+      created_at
+      updated_at
+    }
+  }
+`;
+
+export const SEARCH_EMPLOYEES_QUERY = gql`
+  query SearchEmployees($designation: String, $department: String) {
+    searchEmployees(designation: $designation, department: $department) {
+      id
+      first_name
+      last_name
+      email
+      gender
+      designation
+      salary
+      date_of_joining
+      department
+      employee_photo
+      created_at
+      updated_at
+    }
+  }
+`;
+
+export const ADD_EMPLOYEE_MUTATION = gql`
+  mutation AddEmployee($input: AddEmployeeInput!) {
+    addEmployee(input: $input) {
+      id
+      first_name
+      last_name
+      email
+      gender
+      designation
+      salary
+      date_of_joining
+      department
+      employee_photo
+    }
+  }
+`;
+
+export const UPDATE_EMPLOYEE_MUTATION = gql`
+  mutation UpdateEmployee($id: ID!, $input: UpdateEmployeeInput!) {
+    updateEmployee(id: $id, input: $input) {
+      id
+      first_name
+      last_name
+      email
+      gender
+      designation
+      salary
+      date_of_joining
+      department
+      employee_photo
+    }
+  }
+`;
+
+export const DELETE_EMPLOYEE_MUTATION = gql`
+  mutation DeleteEmployee($id: ID!) {
+    deleteEmployee(id: $id) {
+      id
+    }
+  }
+`;
